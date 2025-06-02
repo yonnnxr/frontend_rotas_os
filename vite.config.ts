1e3fd7783887bb1feb2c105da6db9fd7d56dae22
yonnnxr <carlosborges2007edu@gmail.com>
2025-06-02 17:30:42 -0400

    refactor: [Atualização da configuração do Vite e ajustes no workflow de Jekyll] - Alterado o caminho base no Vite para '/' para simplificar a configuração do projeto. Adicionada a etapa de configuração de domínio personalizado no workflow de Jekyll, criando um arquivo CNAME para facilitar a implementação do domínio.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Otimizador_de_rotas/',
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: true,
