3f9fb874d30b559046df1c24768ab8b6386b7ec7
yonnnxr <carlosborges2007edu@gmail.com>
2025-06-02 15:23:58 -0400

    fix: [Removendo import não utilizado do Fragment]

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,4 +1,3 @@
-import { Fragment } from 'react'
 import { Disclosure } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { Outlet } from 'react-router-dom'
