1a916a01adb4f210b261d0c8e2eb9ab056c5b6d3
yonnnxr <carlosborges2007edu@gmail.com>
2025-06-02 17:01:34 -0400

    fix: [Atualização da configuração do Vite e ajustes no workflow de deploy] - Alterado o caminho base no Vite para '/Otimizador_de_rotas/' e removido o arquivo de workflow de deploy anterior. Além disso, atualizado o workflow de Jekyll para usar a branch 'main' e ajustadas as etapas de instalação e build para o novo diretório do frontend.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/frontend_rotas_os/',
+  base: '/Otimizador_de_rotas/',
   build: {
     outDir: 'dist',
     sourcemap: true,
