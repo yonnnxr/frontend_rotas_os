3bd735da2546d5d48a1d1f63938bdb1db159f82d
yonnnxr <carlosborges2007edu@gmail.com>
2025-06-03 14:38:56 -0400

    refactor: [Atualização da configuração do Vite] - Alterado o caminho base no Vite para '/' para simplificar a configuração do projeto, facilitando a implementação e a manutenção futura.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Otimizador_de_rotas/',
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: true,
